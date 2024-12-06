07b4549f5ea0445180760edf8f9eff8ed129475b
owen-makes <owen.botto@gmail.com>
2024-12-05 22:59:44 -0300

    Add basic CRUD actions + views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,49 @@
 class KittensController < ApplicationController
+  before_action :set_kitten, only: [ :show, :edit, :update, :destroy ]
   def index
+    @kittens = Kitten.all
   end
 
   def show
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten, notice: "Kitten was created"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: "Succesfully updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @kitten.destroy
+    redirect_to kittens_url, notice: "Kitten destroyed!"
+  end
+
+  private
+
+  def set_kitten
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
